9f1182a33e7ed317182b3baddd84208fa7e3b55d
Sanma <LTHNhung0626@gmail.com>
2017-04-07 00:50:25 +0700

    constructor DeliveringMap() updated

--- src/vn/edu/tdt/it/dsa/DeliveringMap.java
+++ src/vn/edu/tdt/it/dsa/DeliveringMap.java
@@ -2,12 +2,35 @@ package vn.edu.tdt.it.dsa;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class DeliveringMap {
 
+	private int[][] undirectedGraph = new int[100][100];
+	private int[][] directedGraph = new int[100][100];
+
 	public DeliveringMap(File file) throws IOException{
 		//sinh vien viet ma o day
+		Scanner sc = new Scanner(file);
+		String content = "";
+		while (sc.hasNext()) {
+			content += sc.next();
+		}
+		List<String> list = new ArrayList<>(Arrays.asList(content.split("\\s+")));
+		for (int i=0; i < list.size(); i ++) {
+			int weight = Integer.parseInt(list.get(i).substring(2,5));
+			int src = Integer.parseInt(list.get(i).substring(0,2));
+			int dest = Integer.parseInt(list.get(i).substring(5,7));
+
+			undirectedGraph[src][dest] = weight;
+			undirectedGraph[dest][src] = weight;
+			directedGraph[src][dest] = weight;
+		}
 	}
+
 	
 	public int calculate(int level, boolean rushHour){
 		int res = 0;
