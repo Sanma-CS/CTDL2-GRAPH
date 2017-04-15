45b8756e1e613ba1c7708a74d64d35367e3ac835
Walpurisnacht <walpurisnacht.klarissa@gmail.c>
2017-04-15 17:58:55 +0700

    CTDL2: - Fix Prism Algorithm to work with current approach - DFS tweak

--- src/vn/edu/tdt/it/dsa/DeliveringMap.java
+++ src/vn/edu/tdt/it/dsa/DeliveringMap.java
@@ -49,7 +49,7 @@ public class DeliveringMap {
 		sumEdgesVertices += vertices.size();
 	}
 
-	
+
 	public int calculate(int level, boolean rushHour){
 		int res = 0;
 		//sinh vien viet ma o day
@@ -64,9 +64,7 @@ public class DeliveringMap {
 		visited[src] = true;
 		path.add(src);
 		if (src == dest) {
-			paths.add(new ArrayList<Integer>(path));
-			//path.remove(path.size()-1);
-			//return;
+			paths.add(new ArrayList<>(path));
 		}
 		else {
 			//System.out.println(src + " ");	//debug
@@ -77,79 +75,130 @@ public class DeliveringMap {
 					}
 				}
 			}
-			//path.remove(path.size()-1);
+
 		}
 		path.remove(path.size()-1);
 		visited[src] = false;
-
 	}
 
-	public void DFS (int src, int dest, int graph[][]) {
+	private void DFS (int src, int dest, int graph[][]) {
 		boolean visited[] = new boolean[MAX_EDGE];
 		DFSUtil(src, dest, graph, visited);
 	}
 
-	private int minkey(int key[], boolean mstSet[]) {
-		int min_index = -1;
-		int min = Integer.MAX_VALUE;
-		for (int i = 0; i < MAX_EDGE; i++) {
-			if(!mstSet[i] && key[i] < min) {
-				min = key[i];
-				min_index = i;
-				//System.out.println(min_index);
-			}
-		}
+    private int minKey(int key[], boolean mstSet[])
+    {
+        // Initialize min value
+        int min = Integer.MAX_VALUE, min_index=-1;
 
-		return min_index;
-	}
+        for (int v = 0; v < MAX_EDGE; v++)
+            if (mstSet[v] == false && key[v] < min)
+            {
+                min = key[v];
+                min_index = v;
+            }
 
-	private void primMST (int g[][]) {
-		int parent[] = new int[MAX_EDGE];
-		int key[] = new int[MAX_EDGE];
-		boolean mstSet[] = new boolean[MAX_EDGE]; // To represent set of vertices not yet included in MST
+        return min_index;
+    }
 
-		for (int i = 0; i < MAX_EDGE; i++) {
-			key[i] = Integer.MAX_VALUE;
-		}
-		key[0] = 0; // Make key 0 so that this vertex is picked as first vertex
-		parent[0] = -1; // First node is always root of MST
+    // A utility function to print the constructed MST stored in
+    // parent[]
+    private void printMST(int parent[], int graph[][])
+    {
+        System.out.println("Edge   Weight");
+        for (int i = 0; i < MAX_EDGE; i++) {
+            if (parent[i] < 0 || graph[i][parent[i]] == Integer.MAX_VALUE) continue;
+            System.out.println(parent[i] + " - " + i + "    " +
+                    graph[i][parent[i]]);
+        }
+    }
 
-		for (int j = 0; j < MAX_EDGE - 1; j++) {
-			int u = minkey(key, mstSet);
-			if (u < 0) continue;
-			mstSet[u] = true;
-			//System.out.println(minkey(key, mstSet));
+    // Function to construct and print MST for a graph represented
+    //  using adjacency matrix representation
+    private void primMST(int graph[][])
+    {
+        // Array to store constructed MST
+        int parent[] = new int[MAX_EDGE];
 
-			for (int v = 0; v < MAX_EDGE; v++) {
-				if (g[u][v] != Integer.MAX_VALUE && !mstSet[v] && g[u][v] < key[v]) {
-					parent[v] = u;
-					key[v] = g[u][v];
-					System.out.println(g[minkey(key, mstSet)][v]);
-				}
-			}
-		}
-		//printMST(parent, g);
-	}
+        // Key values used to pick minimum weight edge in cut
+        int key[] = new int [MAX_EDGE];
 
-	//to debug primMST
-	private void printMST (int parent[], int g[][]) {
-		System.out.println("edge	weight");
-		for (int i = 0; i < MAX_EDGE; i++) {
-			if (parent[i] != -1)
-				System.out.println(parent[i] + " - " + i + " " + g[i][parent[i]]);
-		}
-	}
+        // To represent set of vertices not yet included in MST
+        boolean mstSet[] = new boolean[MAX_EDGE];
 
-	
+        // Initialize all keys as INFINITE
+        for (int i = 0; i < MAX_EDGE; i++)
+        {
+            if (isContainsEdge(graph, i)) {
+                key[i] = Integer.MAX_VALUE;
+            } else {
+                key[i] = 0;
+            }
+        }
+
+        // Always include first 1st vertex in MST.
+        key[findFirstNode(graph)] = 0;     // Make key 0 so that this vertex is
+        // picked as first vertex
+        parent[findFirstNode(graph)] = -1; // First node is always root of MST
+
+        // The MST will have V vertices
+        for (int count = 0; count < MAX_EDGE-1; count++)
+        {
+            // Pick thd minimum key vertex from the set of vertices
+            // not yet included in MST
+            int u = minKey(key, mstSet);
+
+            // Add the picked vertex to the MST Set
+            if (u < 0) continue;
+            mstSet[u] = true;
+
+            // Update key value and parent index of the adjacent
+            // vertices of the picked vertex. Consider only those
+            // vertices which are not yet included in MST
+            for (int v = 0; v < MAX_EDGE; v++)
+
+                // graph[u][v] is non zero only for adjacent vertices of m
+                // mstSet[v] is false for vertices not yet included in MST
+                // Update the key only if graph[u][v] is smaller than key[v]
+                if (graph[u][v]!=Integer.MAX_VALUE && mstSet[v] == false &&
+                        graph[u][v] <  key[v])
+                {
+                    parent[v]  = u;
+                    key[v] = graph[u][v];
+                }
+        }
+
+        // print the constructed MST
+        printMST(parent, graph);
+    }
+
+    private int findFirstNode(int[][] graph) {
+        for (int i = 0; i < MAX_EDGE; i++) {
+            for (int j = 0; j < MAX_EDGE; j++) {
+                if (graph[i][j] > 0 && graph[i][j] != Integer.MAX_VALUE)
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isContainsEdge(int[][] graph, int src) {
+        for (int i = 0; i < MAX_EDGE; i++) {
+            if (graph[src][i] == Integer.MAX_VALUE || graph[i][src] ==
+                    Integer.MAX_VALUE) continue;
+            if (graph[src][i] > 0 || graph[i][src] > 0) return true;
+        }
+        return false;
+    }
+
 	public static void main (String[] args){
 		try{
 			DeliveringMap map = new DeliveringMap(new File("map.txt"));
 			//System.out.println(map.calculate(3, false));		//default do not touch
 			//System.out.println(map.case_1(1));	//debug
-			//map.DFS(1, 7, map.undirectedGraph);
-			//System.out.println(map.path);
-			//System.out.println(map.paths);
-			map.primMST(map.undirectedGraph);
+			map.DFS(1, 7, map.undirectedGraph);
+			System.out.println(map.path);
+			System.out.println(map.paths);
 		}catch(Exception ex){
 			ex.printStackTrace();
 		}
